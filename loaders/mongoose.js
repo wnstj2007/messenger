c6236b7564507a9b6c0c87c9ba6d8cdeedb20f7d
wnstj2007 <wnstj2096@gmail.com>
2021-08-04 23:53:29 +0900

    load mongoose

---
+++ loaders/mongoose.js
@@ -0,0 +1,10 @@
+var db = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/testdb", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+
+module.exports = db;
