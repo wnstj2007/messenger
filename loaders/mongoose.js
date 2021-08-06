5c0c2b85e5c609ec30ce0a9d83466ec555c925eb
wnstj2007 <wnstj2096@gmail.com>
2021-08-06 20:28:40 +0900

    Modify chatting page

--- loaders/mongoose.js
+++ loaders/mongoose.js
@@ -1,10 +1,12 @@
-var db = require('mongoose');
+var mongoose = require('mongoose');
 
+/*
 mongoose.connect("mongodb://127.0.0.1:27017/testdb", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
     useUnifiedTopology: true,
   });
+*/
 
-module.exports = db;
+module.exports = mongoose;
--- routes/upload.js
+++ routes/upload.js
@@ -4,11 +4,14 @@ const multer = require('multer');
 var db = require('../loaders/mongoose.js')
 
 router.get('/upload', (req, res) => {
+	console.log('render upload page');
 	res.render('upload');
 });
 
+/*
 router.post('/upload', upload.single('uploadedFile'), (req, res) => {
 	res.send('업로드 성공!');
 });
+*/
 
 module.exports = router;
