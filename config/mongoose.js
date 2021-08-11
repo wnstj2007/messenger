7ed713b461ce615a930fe169c0521faf1c4d9423
Junseo Park <wnstj2096@gmail.com>
2021-08-11 23:50:21 +0900

    Add Schema

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const config = require('config');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+mongoose.connect(config.db, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+const File = new Schema({
+  name: String,
+  originalName: String,
+  content: Buffer
+});
+
+module.exports = mongoose;
